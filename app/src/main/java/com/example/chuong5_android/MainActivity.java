960bf4c551f629a1ce1189bed9b8a9ab5e43da68
Nguyễn Lăng Phước An <119149804+NguyenLangPhuocAn@users.noreply.github.com>
2023-05-22 12:32:41 +0700

    Option Menu
    Done :D

--- app/src/main/java/com/example/chuong5_android/MainActivity.java
+++ app/src/main/java/com/example/chuong5_android/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.chuong5_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -24,5 +28,27 @@ public class MainActivity extends AppCompatActivity {
     // Init Widget - Khanhvtrng12
     void InitWidget(){
         btnTst = findViewById(R.id.btn_Toast);
+    }
+    //gán menu vào bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.optionmenu,menu);
+        return true;
+    }
+    //click chon item
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.settings)
+            Toast.makeText(this, "Bạn Đã Chọn Settings", Toast.LENGTH_SHORT).show();
+        if(id==R.id.tool)
+            Toast.makeText(this, "Bạn Đã Chọn Tool", Toast.LENGTH_SHORT).show();
+        if(id==R.id.Exit)
+            Toast.makeText(this, "Bạn Đã Chọn Exit", Toast.LENGTH_SHORT).show();
+        if(id==R.id.thongtin)
+            Toast.makeText(this, "Bạn Đã Chọn Thông Tin Phiên Bản", Toast.LENGTH_SHORT).show();
+        if(id==R.id.qlnv)
+            Toast.makeText(this, "Bạn Đã Chọn quản Lý Nhân Viên", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
